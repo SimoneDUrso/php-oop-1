5783d1bcc6de576af2d73f1a3b1f54dc48edd67d
SimoneDUrso <simone.durso98@gmail.com>
2024-09-17 15:52:41 +0200

    [UPD] Exercise completed.

--- index.php
+++ index.php
@@ -24,7 +24,9 @@ $lotr = new Movie('Lord of the Ring', 'Fantasy, Adventure', '2002-01-18', 'Peter
 $sh = new Movie('Il mistero di Sleepy Hollow', 'Horror, Fantasy, Gothic', '1999-11-17', 'Tim Burton');
 $twows = new Movie('The Wolf of Wall Street', 'Comedy, Black humor, Thriller', '2014-01-23', 'Martin Scorsese');
 
-var_dump($lotr, $sh, $twows)
+echo $lotr->infoMovie() . '<br>' . '<br>';
+echo $sh->infoMovie() . '<br>' . '<br>';
+echo $twows->infoMovie() . '<br>' . '<br>';
 ?>
 
 <!DOCTYPE html>
